60a330c08900c08b5e23da63df0acafb5d4caa2f
Ganeev Singh <ganeevnyc@gmail.com>
2026-05-16 02:55:59 -0400

    Initial Commit

---
+++ Simple Live Wallpaper/Simple_Live_WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Live_WallpaperApp.swift
+//  Simple Live Wallpaper
+//
+//  Created by Ganeev Singh on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Live_WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
